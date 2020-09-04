650b0517a36da1615ac5b0cdab85268cbf2d98bf
safwanvk <safwanvalakundil@gmail.com>
2020-09-04 14:04:50 +0530

    :zap: Set Add category

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, request, flash, url_for
 from shop import app
 from werkzeug.utils import redirect
 
-from .models import Brand
+from .models import Brand, Category
 from .. import db
 
 
@@ -17,3 +17,15 @@ def add_brand():
         flash(f'The brand {get_brand} was added to your database', 'success')
         return redirect(url_for('add_brand'))
     return render_template('products/add_brand.html', brands='brands')
+
+
+@app.route('/add-category', methods=['GET', 'POST'])
+def add_category():
+    if request.method == 'POST':
+        get_category = request.form['category']
+        category = Category(name=get_category)
+        db.session.add(category)
+        db.session.commit()
+        flash(f'The brand {get_category} was added to your database', 'success')
+        return redirect(url_for('add_category'))
+    return render_template('products/add_brand.html')
